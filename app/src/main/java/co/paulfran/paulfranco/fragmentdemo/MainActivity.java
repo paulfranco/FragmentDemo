58937f46cde29f455daefeff3ac327a8b080c947
Paul Franco <paulfranco@me.com>
2018-06-29 00:46:15 -0700

    fixed typo in MainActivity

--- app/src/main/java/co/paulfran/paulfranco/fragmentdemo/MainActivity.java
+++ app/src/main/java/co/paulfran/paulfranco/fragmentdemo/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Preferred Method of adding a fragment is to add it programmatically by following these steps:
         // Steps to add a fragment programmatically
-        // 1. Ininitalize the fragment
+        // 1. Initialize the fragment
         HelloFragment helloFragment = new HelloFragment();
         // 2. Initialize the fragment manager
         FragmentManager manager = getFragmentManager();
