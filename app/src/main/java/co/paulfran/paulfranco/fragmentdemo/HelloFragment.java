2c25e74c560a9e865489b9e5849a7f6aea3c3354
Paul Franco <paulfranco@me.com>
2018-06-29 00:02:58 -0700

    Initial commit

---
+++ app/src/main/java/co/paulfran/paulfranco/fragmentdemo/HelloFragment.java
@@ -0,0 +1,19 @@
+package co.paulfran.paulfranco.fragmentdemo;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HelloFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_hello, container, false);
+        //
+        return view;
+    }
+}
